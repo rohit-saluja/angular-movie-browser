1d3225f0274eabca16df82faa98c01c13c74ba84
Rohit Saluja <saluja730@gmail.com>
2023-07-11 13:38:10 +0530

    updated the header for the website

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, SideNavComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, SideNavComponent],
 })
 export class CoreModule {}
