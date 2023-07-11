944e23c35962d4c51fcfd23afbbb790423a1f01a
Rohit Saluja <saluja730@gmail.com>
2023-07-11 12:45:48 +0530

    landing page is added

--- src/app/feature/landing-page/landing-page.module.ts
+++ src/app/feature/landing-page/landing-page.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingPageComponent
+  ],
   imports: [
     CommonModule,
     LandingPageRoutingModule
