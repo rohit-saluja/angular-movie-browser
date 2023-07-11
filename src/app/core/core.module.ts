b89318a4d625ea5032c8e7fca26ca3b1d60c2598
Rohit Saluja <saluja730@gmail.com>
2023-07-11 12:56:42 +0530

    header is integrated for the landing page

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 
-
-
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    SideNavComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, SideNavComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, SideNavComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
--- src/app/feature/landing-page/landing-page.module.ts
+++ src/app/feature/landing-page/landing-page.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
+import { CoreModule } from 'src/app/core/core.module';
 
 @NgModule({
-  declarations: [
-    LandingPageComponent
-  ],
-  imports: [
-    CommonModule,
-    LandingPageRoutingModule
-  ]
+  declarations: [LandingPageComponent],
+  imports: [CommonModule, LandingPageRoutingModule, CoreModule],
 })
-export class LandingPageModule { }
+export class LandingPageModule {}
