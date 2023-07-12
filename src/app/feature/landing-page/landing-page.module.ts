5ebccb252972563362a8962612e3c8663c41d5a8
Rohit Saluja <saluja730@gmail.com>
2023-07-12 10:18:32 +0530

    imported the banner co the landing page

--- src/app/feature/landing-page/landing-page.module.ts
+++ src/app/feature/landing-page/landing-page.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CoreModule } from 'src/app/core/core.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LandingPageComponent],
-  imports: [CommonModule, LandingPageRoutingModule, CoreModule],
+  imports: [CommonModule, LandingPageRoutingModule, CoreModule, SharedModule],
 })
 export class LandingPageModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 
-
-
 @NgModule({
-  declarations: [
-    BannerComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [BannerComponent],
+  imports: [CommonModule],
+  exports: [BannerComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
