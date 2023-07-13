ba5b902097dfe2eabd2456ab964d96c67b9960fc
Rohit Saluja <saluja730@gmail.com>
2023-07-13 18:54:05 +0530

    updated the service files and controller and

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static("public"));
+const port = 3000;
+app.listen(port, () => console.log("App is started"));
