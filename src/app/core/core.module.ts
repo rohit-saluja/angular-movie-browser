3265c985655f632b7b23d407ad90ae52dab0124c
Rohit Saluja <saluja730@gmail.com>
2023-07-13 17:08:48 +0530

    movie card component is added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { RouterModule } from '@angular/router';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SideNavComponent],
+  declarations: [HeaderComponent, SideNavComponent, MovieCardComponent],
   imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, SideNavComponent],
 })
