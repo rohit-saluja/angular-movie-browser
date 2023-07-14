5474ea55ca4daec956ccb929a0994a14a5c2f7e0
Rohit Saluja <saluja730@gmail.com>
2023-07-14 18:11:32 +0530

    updated the morgan and server js

--- server/src/configs/config.js
+++ server/src/configs/config.js
@@ -1,4 +1,27 @@
-const dotenv = require('dotenv');
-const path = require('path');
-const joi = require('joi');
-dotenv.config()
+const dotenv = require("dotenv");
+const path = require("path");
+const joi = require("joi");
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+const envVarsSchema = joi
+  .object()
+  .keys({
+    NODE_ENV: joi.string().valid("development", "production").required(),
+    PORT: joi.number().default(3000),
+    MONGODB_URL: joi.string().required().description("Mongo db url"),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema.validate(process.env);
+
+if (error) {
+  throw new Error("config validation error " + error.message);
+}
+
+console.log(error);
+console.log(value);
+module.exports = {
+  env: envVars.NODE_ENV,
+  mongoose: {
+    url: envVars.MONGODB_URL,
+  },
+};
