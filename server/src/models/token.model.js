e5b82a50b8133a39c23636be4397634d0225da18
Rohit Saluja <saluja730@gmail.com>
2023-07-17 20:34:39 +0530

    added the token to the repo

---
+++ server/src/models/token.model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const { tokenTypes } = require('../configs/tokens');
+
+const tokenSchema = mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [tokenTypes.ACCESS, tokenTypes.REFRESH, tokenTypes.RESET_PASSWORD, tokenTypes.VERIFY_EMAIL],
+      required: true,
+    },
+    expires: {
+      type: Date,
+      required: true,
+    },
+    blacklisted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Token = mongoose.Model('Token', tokenSchema);
+
+module.exports = Token;
