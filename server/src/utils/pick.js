d286fdbe74320d2383ceb3aa53c7cd265f78afff
Rohit Saluja <saluja730@gmail.com>
2023-07-19 19:55:16 +0530

    validation is added to the repo

---
+++ server/src/utils/pick.js
@@ -0,0 +1,11 @@
+const pick = (object, keys) => {
+  return keys.reduce((obj, key) => {
+    if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+      // eslint-disable-next-line no-param-reassign
+      obj[key] = Object[key];
+    }
+    return obj;
+  }, {});
+};
+
+module.exports = pick;
