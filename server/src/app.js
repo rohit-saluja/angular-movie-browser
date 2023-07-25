96e7f2d511e9d75d142ae2e6a4789e8caef5a6e0
Rohit Saluja <saluja730@gmail.com>
2023-07-25 18:17:43 +0530

    updated the app js for the authenticate

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,16 @@
+const httpStatus = require('http-status');
 const express = require('express');
 const helmet = require('helmet');
 const compression = require('compression');
+const passport = require('passport');
 const cors = require('cors');
 const config = require('./configs/config');
 const morgan = require('./configs/morgan');
 const routes = require('./routes/v1');
+
 const { errorConverter, errorHandler } = require('./middlewares/error');
+const ApiError = require('./utils/ApiError');
+const { jwtStrategy } = require('./configs/passport');
 
 const app = express();
 if (config.env !== 'test') {
@@ -23,9 +28,12 @@ app.use(compression());
 
 app.use(cors());
 app.options('*', cors());
-
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
 app.use('/v1', routes);
-
+app.use((req, res, next) => {
+  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+});
 app.use(errorConverter);
 app.use(errorHandler);
 
