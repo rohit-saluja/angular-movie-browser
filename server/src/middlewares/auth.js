4759e2d504399c4219d61c3dd699b8151793f787
Rohit Saluja <saluja730@gmail.com>
2023-07-25 17:57:07 +0530

    updated the jwt stratety for the token verification

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,5 +1,5 @@
-const httpStatus = require('http-status');
 const passport = require('passport');
+const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const { roleRights } = require('../configs/role');
 
@@ -7,14 +7,19 @@ const verifyCallback = (req, resolve, reject, requiredRights) => async (err, use
   if (err || info || !user) {
     return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
   }
+  req.user = user;
+
   if (requiredRights.length) {
     const userRights = roleRights.get(user.role);
     const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
     if (!hasRequiredRights && req.params.userId !== user.id) {
-      return reject(new ApiError(httpStatus.FORBIDDEN, 'forbidden'));
+      return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
     }
   }
+
+  resolve();
 };
+
 const auth =
   (...requiredRights) =>
   async (req, res, next) => {
