9adc9b103fcf4787bf60e5e3a37906b3d7a50e7a
Rohit Saluja <saluja730@gmail.com>
2023-07-26 16:58:06 +0530

    updated the form for the sign up process

--- src/app/feature/auth/signup/signup.component.ts
+++ src/app/feature/auth/signup/signup.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
+  signUpForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: [Validators.required],
+    });
+  }
+
+  get name() {
+    return this.signUpForm.get('name');
+  }
+
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword');
+  }
 }
