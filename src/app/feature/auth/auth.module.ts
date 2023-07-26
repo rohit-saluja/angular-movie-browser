df1f27c685fed67753d0f30f766bb5cc39bc25a5
Rohit Saluja <saluja730@gmail.com>
2023-07-26 09:56:59 +0530

    added the auth components tot eh repo

--- src/app/feature/auth/auth.module.ts
+++ src/app/feature/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { VerfyEmailComponent } from './verfy-email/verfy-email.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    VerfyEmailComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
---
+++ src/app/feature/auth/verfy-email/verfy-email.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verfy-email',
+  templateUrl: './verfy-email.component.html',
+  styleUrls: ['./verfy-email.component.scss']
+})
+export class VerfyEmailComponent {
+
+}
