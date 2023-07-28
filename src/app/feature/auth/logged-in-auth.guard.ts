74f7c8c13a6234be88b6d7cfe426954fcdef860a
Rohit Saluja <saluja730@gmail.com>
2023-07-28 18:13:51 +0530

    added the reverse auth guard

--- src/app/feature/auth/logged-in-auth.guard.ts
+++ src/app/feature/auth/logged-in-auth.guard.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  CanLoad,
-  Route,
-  Router,
-  UrlSegment,
-  UrlTree,
-} from '@angular/router';
+import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
 import { AuthService } from 'src/app/app/feature/auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoggedInAuthGuard implements CanLoad {
-  constructor(private authService: AuthService,private router:Router) {}\
+  constructor(private authService: AuthService, private router: Router) {}
 
   canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree {
     if (this.authService.isLoggedIn()) {
