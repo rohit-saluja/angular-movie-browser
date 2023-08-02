4a796a4e0a757ebf27e93d951586db39c5cb1f95
Rohit Saluja <saluja730@gmail.com>
2023-08-02 19:46:01 +0530

    updated the header for the componet

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { RouterModule } from '@angular/router';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [HeaderComponent, SideNavComponent, MovieCardComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   exports: [HeaderComponent, SideNavComponent, MovieCardComponent],
 })
 export class CoreModule {}
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/app/feature/auth/auth.service';
+import { User } from 'src/app/feature/auth/user.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  user: User = {};
+  showUserProfilePopup: boolean = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.userObject;
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe();
+  }
 }
