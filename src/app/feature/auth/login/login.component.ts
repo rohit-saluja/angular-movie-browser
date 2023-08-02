eb04a156661203e17507c9b293c2592247e57df7
Rohit Saluja <saluja730@gmail.com>
2023-08-02 20:04:30 +0530

    login is added to the form

--- src/app/feature/auth/login/login.component.ts
+++ src/app/feature/auth/login/login.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/app/feature/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  showPassword: boolean = false;
+  loginForm: FormGroup = new FormGroup({});
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      passwor: ['', [Validators.required]],
+    });
+  }
+
+  login(): void {
+    this.authService.login(this.loginForm.value).subscribe(() => {});
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
