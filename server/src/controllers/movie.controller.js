1e751e5dc92c6ffad1bcb65f3c54bf62f0b438ef
Rohit Saluja <saluja730@gmail.com>
2023-08-23 20:13:30 +0530

    updated the default case for the movies

--- server/src/controllers/movie.controller.js
+++ server/src/controllers/movie.controller.js
@@ -7,6 +7,10 @@ const getMovies = async (req, res) => {
 
 const searchMovies = async (req, res) => {
   const { categories } = req.body;
+  if (!categories.length) {
+    const movies = await Movie.find({});
+    res.send(movies);
+  }
   const categoriesDoc = await Category.find({ name: { $in: categories } });
   const categoriesIds = categoriesDoc.map((c) => c._id);
   const movies = await Movie.find({ category: { $in: categoriesIds } });
