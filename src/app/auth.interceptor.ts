6ccd2ab331e4cf33794b0de4ed32c7160c8ae1cb
Rohit Saluja <saluja730@gmail.com>
2023-08-29 16:10:47 +0530

    log out is added in the repo

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -8,6 +8,7 @@ import {
 import { Observable } from 'rxjs';
 import { Token } from './feature/auth/token.model';
 import { AuthService } from './app/feature/auth/auth.service';
+import * as moment from 'moment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -19,6 +20,9 @@ export class AuthInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<unknown>> {
     if (localStorage.getItem('token')) {
       const token: Token = JSON.parse(localStorage.getItem('token') || '');
+      if (moment(token.access.expires).isSameOrBefore(moment.now())) {
+        this.authService.logout().subscribe();
+      }
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token.access.token}`,
